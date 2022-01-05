2a5fdd86eb738483001f51250ecd33ac4113af90
Darling <782775618@qq.com>
2022-01-05 10:11:42 +0800

    啥也没干

---
+++ demo/src/main/java/com/cntytz/web/SsoController.java
@@ -0,0 +1,15 @@
+package com.cntytz.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: luban
+ * @description:
+ * @author: ling
+ * @create: 2020-05-22 13:52
+ **/
+@RestController
+@RequestMapping("login")
+public class SsoController {
+}
